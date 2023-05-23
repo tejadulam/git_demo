59709810087cf5b9b380fc8e250abc47cab92244
tejadulam <tejunld2001@gmial.com>
2023-05-23 10:26:04 +0530

    added docsting to sample.py

--- sample.py
+++ sample.py
@@ -4,6 +4,8 @@ a = int(input("a: "))
 b = int(input("b: "))
 
 def exponential_value(x, y):
+    """This function will help us to 
+    return the exponential value"""
     return pow(x, y)
 
 if __name__ == "__main__":
